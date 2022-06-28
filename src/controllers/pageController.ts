504b601d9c00ba9d9b3931ddd95a47a78a18ae7d
Emanuel Barbosa <emanuellbarbosa@hotmail.com>
2022-06-28 10:40:40 -0300

    modificando dados da pagina

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,14 +1,34 @@
-import {Request, Response} from 'express';
+import { Request, Response } from "express";
 
-export const home = (req:Request, res:Response) =>{
-
-}
-export const dogs = (req:Request, res:Response) =>{
-
-}
-export const cats = (req:Request, res:Response) =>{
-
-}
-export const fishes = (req:Request, res:Response) =>{
-
-}
+export const home = (req: Request, res: Response) => {
+  res.render("pages/pages",{
+    banner:{
+        title: "Todos os animais",
+        background: 'allanimals.jpg'
+    }
+  });
+};
+export const dogs = (req: Request, res: Response) => {
+    res.render("pages/pages",{
+        banner:{
+            title: "Cachorros",
+            background: 'banner_dog.jpg'
+        }
+      });
+};
+export const cats = (req: Request, res: Response) => {
+    res.render("pages/pages",{
+        banner:{
+            title: "Gatos",
+            background: 'banner_cat.jpg'
+        }
+      });
+};
+export const fishes = (req: Request, res: Response) => {
+    res.render("pages/pages",{
+        banner:{
+            title: "Peixes",
+            background: 'banner_fish.jpg'
+        }
+      });
+};
