8b3e7674f58318978f31ae4b71131c9e8804ff77
Emanuel Barbosa <emanuellbarbosa@hotmail.com>
2022-07-03 20:19:25 -0300

    projeto finalizado

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -1,5 +1,17 @@
-import {Request, Response} from 'express';
+import { query, Request, Response } from "express";
+import { pet } from "../models/Pet";
+import { createMenuObject } from "../helpers/createMenuObject";
 
-export const search = (req:Request, res:Response) =>{
-
-}
+export const search = (req: Request, res: Response) => {
+  let query: string = req.query.q as string;
+  if (!query) {
+    res.redirect("/");
+    return;
+  }
+  let list = pet.getFromName(query);
+  res.render("pages/pages", {
+    menu: createMenuObject(""),
+    list,
+    query,
+  });
+};
--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import mustache from "mustache-express";
 import path from "path";
-import mainRoutes from './routes/index';
+import mainRoutes from "./routes/index";
 
 dotenv.config();
 const server = express();
@@ -14,7 +14,7 @@ server.use(express.static(path.join(__dirname, "../public")));
 
 server.use(mainRoutes);
 server.use((req, res) => {
-  res.send("pagina nao encontrada");
+  res.render("pages/404");
 });
 //rotas
 server.listen(process.env.PORT);
